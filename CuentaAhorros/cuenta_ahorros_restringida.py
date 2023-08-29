439b8d71d5529bfdd7fa48b78f66bff654698282
Abel García Nájera <abel.garcia.n@gmail.com>
2023-08-29 10:27:02 -0600

    Update cuenta_ahorros_restringida.py

--- CuentaAhorros/cuenta_ahorros_restringida.py
+++ CuentaAhorros/cuenta_ahorros_restringida.py
@@ -7,21 +7,13 @@ class CuentaAhorrosRestringida(CuentaAhorros):
     def __init__(self, nombre, nip, saldo = 0.0):
         """Mismo atributos que CuentaAhorros, pero con un contador
         para los retiros."""
-        CuentaAhorros.__init__(self, nombre, nip, saldo)
-        self.__contador = 0
-        
+
+                    
         
     def retirar(self, cantidad):
         """Restringe el número de retiros a RETIROS_MAX."""
-        if self.__contador == CuentaAhorrosRestringida.RETIROS_MAX:
-            return "No tienes más retiros disponibles este mes"
-        else:
-            mensaje = super().retirar(cantidad)
-            if mensaje == None:
-                self.__contador += 1
-            return mensaje
            
            
     def restablecer_contador(self):
         """Restablece el contador de retiros."""
-        self.__contador = 0
+        
